9a19c09a578aa962458521ad2d5329ad9d55c316
mts0629 <soratobi93@gmail.com>
2024-02-22 00:44:01 +0900

    Activate CuPy with an environment variable

--- deep-learning-from-scratch-2/common/config.py
+++ deep-learning-from-scratch-2/common/config.py
@@ -1,2 +1,7 @@
-# True when using the CuPy
-GPU = False
+import os
+
+# Activate CuPy with environment variable "USE_GPU"
+if os.environ.get("USE_GPU") == "1":
+    GPU = True
+else:
+    GPU = False

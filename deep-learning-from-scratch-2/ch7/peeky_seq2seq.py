ba6f1863d03fc602f8356fb387ba2c39f697cb11
mts0629 <soratobi93@gmail.com>
2024-03-14 23:47:59 +0900

    Book #2: 7.4

---
+++ deep-learning-from-scratch-2/ch7/peeky_seq2seq.py
@@ -0,0 +1,99 @@
+import sys
+sys.path.append("..")
+
+from common.time_layers import *
+from seq2seq import Seq2seq, Encoder
+
+
+class PeekyDecoder:
+    """ Seq2seq peeky decoder 
+    """
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+
+        embed_W = (rn(V, D) / 100).astype("f")
+        lstm_Wx = (rn(H + D, 4 * H) / np.sqrt(H + D)).astype("f")
+        lstm_Wh = (rn(H, 4 * H) / np.sqrt(H)).astype("f")
+        lstm_b = np.zeros(4 * H).astype("f")
+        affine_W = (rn(H + H, V) / np.sqrt(H + H)).astype("f")
+        affine_b = np.zeros(V).astype("f")
+
+        self.embed = TimeEmbedding(embed_W)
+        self.lstm = TimeLSTM(lstm_Wx, lstm_Wh, lstm_b, stateful=True)  # Hidden state is inheritted
+        self.affine = TimeAffine(affine_W, affine_b)
+
+        self.params, self.grads = [], []
+        for layer in (self.embed, self.lstm, self.affine):
+            self.params += layer.params
+            self.grads += layer.grads
+        self.cache = None
+
+    def forward(self, xs, h):
+        N, T = xs.shape
+        N, H = h.shape
+
+        self.lstm.set_state(h)
+
+        out = self.embed.forward(xs)
+        hs = np.repeat(h, T, axis=0).reshape(N, T, H)
+        out = np.concatenate((hs, out), axis=2)  # Concat the hidden state vector
+
+        out = self.lstm.forward(out)
+        out = np.concatenate((hs, out), axis=2)
+
+        score = self.affine.forward(out)
+        self.cache = H
+        return score
+
+    def backward(self, dscore):
+        H = self.cache
+
+        dout = self.affine.backward(dscore)
+        dout, dhs0, = dout[:, :, H:], dout[:, :, :H]
+        dout = self.lstm.backward(dout)
+        dembed, dhs1 = dout[:, :, H:], dout[:, :, :H]
+        self.embed.backward(dembed)
+
+        # Grad of the hidden state of LSTM
+        dhs = dhs0 + dhs1
+        dh = self.lstm.dh + np.sum(dhs, axis=1)
+        return dh
+
+    def generate(self, h, start_id, sample_size):
+        """ Generate a sentence
+        """
+        sampled = []
+        sample_id = start_id
+        self.lstm.set_state(h)
+
+        H = h.shape[1]
+        peeky_h = h.reshape(1, 1, H)  # Hidden state vector
+
+        # Generate word ids sequentially
+        for _ in range(sample_size):
+            x = np.array([sample_id]).reshape((1, 1))
+            out = self.embed.forward(x)
+
+            out = np.concatenate((peeky_h, out), axis=2)  # Concat the hidden state vector
+            out = self.lstm.forward(out)
+            out = np.concatenate((peeky_h, out), axis=2)
+            score = self.affine.forward(out)
+
+            sample_id = np.argmax(score.flatten())
+            sampled.append(sample_id)
+
+        return sampled
+
+
+class PeekySeq2seq(Seq2seq):
+    """ Seq2seq model with a peeky decoder
+    """
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        self.encoder = Encoder(V, D, H)
+        self.decoder = PeekyDecoder(V, D, H)
+        self.softmax = TimeSoftmaxWithLoss()
+
+        self.params = self.encoder.params + self.decoder.params
+        self.grads = self.encoder.grads + self.decoder.grads
--- deep-learning-from-scratch-2/ch7/train_seq2seq.py
+++ deep-learning-from-scratch-2/ch7/train_seq2seq.py
@@ -8,14 +8,19 @@ from dataset import sequence
 from common.optimizer import Adam
 from common.trainer import Trainer
 from common.util import eval_seq2seq
-from seq2seq import Seq2seq
-# from peeky_seq2seq import PeekySeq2seq
+# from seq2seq import Seq2seq
+from peeky_seq2seq import PeekySeq2seq
 
 
 def main():
     # Load a dataset
     (x_train, t_train), (x_test, t_test) = sequence.load_data("addition.txt")
     char_to_id, id_to_char = sequence.get_vocab()
+
+    # Reverse the input
+    is_reverse = True
+    if is_reverse:
+        x_train, x_test = x_train[:, ::-1], x_test[:, ::-1]
 
     # Hyperparameters
     vocab_size = len(char_to_id)
@@ -26,7 +31,8 @@ def main():
     max_grad = 5.0
 
     # Model, optimizer and trainer
-    model = Seq2seq(vocab_size, wordvec_size, hidden_size)
+    # model = Seq2seq(vocab_size, wordvec_size, hidden_size)
+    model = PeekySeq2seq(vocab_size, wordvec_size, hidden_size)
     optimizer = Adam()
     trainer = Trainer(model, optimizer)
 
@@ -53,7 +59,9 @@ def main():
     plt.ylabel("accuracy")
     plt.ylim(0, 1.0)
     plt.show()
-    plt.savefig("accuracy.png")
+    # plt.savefig("accuracy.png")
+    # plt.savefig("accuracy_reversed.png")
+    plt.savefig("accuracy_reverse_and_peeky.png")
 
 
 if __name__ == "__main__":

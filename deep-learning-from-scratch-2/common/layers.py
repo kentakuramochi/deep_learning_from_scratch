b67b50278bb75a683eba9287f1bf0afae7ad26f7
mts0629 <soratobi93@gmail.com>
2024-03-17 16:22:21 +0900

    Book #2: 8.3

--- deep-learning-from-scratch-2/common/layers.py
+++ deep-learning-from-scratch-2/common/layers.py
@@ -67,6 +67,24 @@ class Affine:
         return dx
 
 
+class Softmax:
+    """ Simple softmax layer
+    """
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        self.out = softmax(x)
+        return self.out
+
+    def backward(self, dout):
+        dx = self.out * dout
+        sumdx = np.sum(dx, axis=1, keepdims=True)
+        dx -= self.out * sumdx
+        return dx
+
+
 class SoftmaxWithLoss:
     """ Softmax with loss layer
     """

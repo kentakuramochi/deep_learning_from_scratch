11aacac04a4ac2b952224fd40d5a2c183463ac41
mts0629 <soratobi93@gmail.com>
2024-04-17 23:44:43 +0900

    Book #3: step 51, training MNIST

--- book_3/dezero/functions.py
+++ book_3/dezero/functions.py
@@ -250,6 +250,22 @@ def softmax_simple(x, axis=1):
     return y / sum_y
 
 
+class ReLU(Function):
+    def forward(self, x):
+        y = np.maximum(x, 0.0)
+        return y
+
+    def backward(self, gy):
+        (x,) = self.inputs
+        mask = x.data > 0
+        gx = gy * mask
+        return gx
+
+
+def relu(x):
+    return ReLU()(x)
+
+
 class Softmax(Function):
     def __init__(self, axis=1):
         self.axis = axis

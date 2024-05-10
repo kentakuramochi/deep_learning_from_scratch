cd15bd00983ac0f9746ee792224cffe8155f5b9b
mts0629 <soratobi93@gmail.com>
2024-05-10 23:59:37 +0900

    Book #4: chapter 3, bellman optimality equation

--- book_4/ch03/bellman_equation.py
+++ book_4/ch03/bellman_equation.py
@@ -113,15 +113,33 @@ def transit(state, action):
 # v(L1) = -2.25
 # v(L2) = -2.75
 
-# Bellman equations for action-value function
+# Bellman equations for action-value function (Q function), in deterministic transision
 #
 # q(L1, Right) =
-#   0.5 * (
-#       r(L1, Right, L1)(=0)
-#       + discount_rate * (0.5 * q(L1, Right) + 0.5 * q(L1, Left))
-#  )
-# + 0.5 * (
-#       r(L1, Right, L2)(=1)
-#       + discount_rate * (0.5 * q(L2, Right) + 0.5 * q(L1, Left))
+#   (r(L1, Right, L1)(=0)
+#    + discount_rate * (0.5 * q(L1, Right) + 0.5 * q(L1, Left))
 #   )
+#   + (r(L1, Right, L2)(=1)
+#      + discount_rate * (0.5 * q(L2, Right) + 0.5 * q(L1, Left))
+#     )
 # ...
+
+# Solutions of the bellman optimality equation
+#
+# v*(L1) = 5.26
+# v*(L2) = 4.73
+
+vL1 = 5.26
+vL2 = 4.73
+
+discount_rate = 0.9
+
+q_l1_l = reward(State.L1, Action.Left, State.L1) + discount_rate * vL1
+q_l1_r = reward(State.L1, Action.Right, State.L2) + discount_rate * vL1
+print(f"q(L1, L)={q_l1_l}, q(L1, R)={q_l1_r}")
+print(f"Optimal policy: mu*(L1)={Action.Left if q_l1_l > q_l1_r else Action.Right}")
+
+q_l2_l = reward(State.L2, Action.Left, State.L1) + discount_rate * vL2
+q_l2_r = reward(State.L2, Action.Right, State.L2) + discount_rate * vL2
+print(f"q(L2, L)={q_l2_l}, q(L2, R)={q_l2_r}")
+print(f"Optimal policy: mu*(L2)={Action.Left if q_l2_l > q_l2_r else Action.Right}")
